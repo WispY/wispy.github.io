07d04b7c0904daf34ebcfdbdbd29fd46f43e0c6c
Poliakov Maksym <polmax09@gmail.com>
2019-08-08 23:13:22 +0300

    Test React app on github pages

---
+++ precache-manifest.bfafcf67d45ade6f82d512b68d370410.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d76a27e414b7f7dd7d23a1cddea1f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e785279e08922a65d7c",
+    "url": "/static/js/2.e1e8586c.chunk.js"
+  },
+  {
+    "revision": "4737e815ac1a78109e1a",
+    "url": "/static/js/main.758d5eae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
